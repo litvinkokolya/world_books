4f48201ee309d85b79396050154f61b14ad8de68
Николай <92692339+litvinkokolya@users.noreply.github.com>
2023-04-09 22:33:14 +0300

    Add files via upload

---
+++ world_books/WebBooks/catalog/views.py
@@ -0,0 +1,90 @@
+from django.http import HttpResponseRedirect, HttpResponseNotFound
+from django.shortcuts import render
+from .forms import AuthorsForm
+from .models import Book, Author, Bookinstance
+from django.views import generic
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+
+class ВookListView(generic.ListView):
+    model = Book
+    paginate_by = 3
+
+class BookDetailView(generic.DetailView):
+    model = Book
+
+class AuthorListView(generic.ListView):
+    model = Author
+    paginate_by = 4
+
+class LoanedBooksByUserListView(LoginRequiredMixin, generic.ListView):
+    model = Bookinstance
+    template_name = 'catalog/bookinstance_list_borrowed_user.html'
+    paginate_by = 10
+    def get_queryset(self):
+        return Bookinstance.objects.filter(borrower=self.request.user.filter(status_exact='2').order_by('due_back'))
+
+def index(request):
+    num_books = Book.objects.all().count()
+    num_instances = Bookinstance.objects.all().count()
+    num_instances_available = Bookinstance.objects.filter().count()
+    num_authors = Author.objects.count()
+    num_visits = request.session.get('num_visits', 0)
+    request.session['num_visits'] = num_visits + 1
+    return render(request, 'index.html',
+                  context={'num_books': num_books,
+                           'num_instances': num_instances,
+                           'num_instances_abailable': num_instances_available,
+                           'num_authors': num_authors,
+                           'num_visits': num_visits})
+
+def authors_add(request):
+    author = Author.objects.all()
+    authorsform = AuthorsForm()
+    return render(request, "catalog/authors_add.html", {"form": authorsform, "author": author})
+
+def create(request):
+    if request.method == "POST":
+        author = Author()
+        author.first_name = request.POST.get("first_name")
+        author.last_name = request.POST.get("last_name")
+        author.date_of_birth = request.POST.get("date_of_birth")
+        author.date_of_death = request.POST.get("date_of_death")
+        author.save()
+        return HttpResponseRedirect("/authors_add/")
+
+def delete(request, id):
+    try:
+        author = Author.objects.get(id=id)
+        author.delete()
+        return HttpResponseRedirect("/authors_add/")
+    except Author.DoesNotExist:
+        return HttpResponseNotFound("<h2> Автор не найден </h2>")
+
+def editl(request, id):
+    author = Author.objects.get(id=id)
+    if request.method == "POST":
+        author = Author()
+        author.first_name = request.POST.get("first_name")
+        author.last_name = request.POST.get("last_name")
+        author.date_of_birth = request.POST.get("date_of_birth")
+        author.date_of_death = request.POST.get("date_of_death")
+        author.save()
+        return HttpResponseRedirect("/authors_add/")
+    else:
+        return render(request, "editl.html", {"author": author})
+
+class BookCreate(CreateView):
+    model = Book
+    fields = '__all__'
+    success_url = reverse_lazy('books')
+
+class BookUpdate(UpdateView):
+    model = Book
+    fields = '__all__'
+    success_url = reverse_lazy('books')
+
+class BookDelete(UpdateView):
+    model = Book
+    success_url = reverse_lazy('books')
